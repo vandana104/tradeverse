2972d4000a5533e63c5bd7061557a11fe66a5644
vandana104 <vandanaoel@gmail.com>
2024-02-12 12:20:12 +0530

    Updated

--- src/Components/Donut.js
+++ src/Components/Donut.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, Inject, PieSeries } from '@syncfusion/ej2-react-charts';
-import { Typography } from '@mui/material';
+import React from "react";
+import {
+  AccumulationChartComponent,
+  AccumulationSeriesCollectionDirective,
+  AccumulationSeriesDirective,
+  Inject,
+  PieSeries,
+} from "@syncfusion/ej2-react-charts";
 
 const Donut = () => {
   const data1 = [
-    { x: 'United States', y: 45, text: 'USA', fill: '#00226C' },
-    { x: 'Australia', y: 53, text: 'AUS: 14%', fill: '#0450C2' },
-    { x: 'China', y: 56, text: 'CHN', fill: '#0073DC' },
-    { x: 'India', y: 61, text: 'IND', fill: '#0D98FF' },
-    { x: 'Japan', y: 40, text: 'JPN', fill: '#9CD9FF' },
-    { x: 'United Kingdom', y: 20, text: 'UK', fill: '#0450C2' }
+    { x: "United States", y: 45, text: "USA", fill: "#00226C" },
+    { x: "Australia", y: 53, text: "AUS: 14%", fill: "#0450C2" },
+    { x: "China", y: 56, text: "CHN", fill: "#0073DC" },
+    { x: "India", y: 61, text: "IND", fill: "#0D98FF" },
+    { x: "Japan", y: 40, text: "JPN", fill: "#9CD9FF" },
+    { x: "United Kingdom", y: 20, text: "UK", fill: "#0450C2" },
   ];
 
   return (
-    <AccumulationChartComponent id='charts'>
-        {/* <Typography>hbuxybd,bfmki</Typography> */}
+    <AccumulationChartComponent id="charts">
       <Inject services={[PieSeries]} />
       <AccumulationSeriesCollectionDirective>
-        <AccumulationSeriesDirective dataSource={data1} xName='x' yName='y' type='Pie' />
+        <AccumulationSeriesDirective
+          dataSource={data1}
+          xName="x"
+          yName="y"
+          type="Pie"
+        />
       </AccumulationSeriesCollectionDirective>
     </AccumulationChartComponent>
   );
